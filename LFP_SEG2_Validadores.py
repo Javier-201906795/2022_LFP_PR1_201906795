762a5596ade0b2a9fcf0d35b8a1d4c6e52ef0c90
Javier-201906795 <88503776+Javier-201906795@users.noreply.github.com>
2022-08-20 15:35:02 -0600

    fase 1 Validador Codigo
    funcion de validacion codigo cursos

--- LFP_SEG2_Validadores.py
+++ LFP_SEG2_Validadores.py
@@ -23,9 +23,9 @@ def validadorcamposextra(_listaElementos):
             #──O────────────────O─
             #Valida si no faltan campos
             if (NoCampos < 7):
-                mensaje += "Error en la linea No. " + str(i) + " Faltan Campos; Campos encontrados: "+ str(NoCampos) + "\n"
+                mensaje += "Error en la linea No. " + str(i+1) + " Faltan Campos; Campos encontrados: "+ str(NoCampos) + "\n"
             elif (NoCampos > 7):
-                mensaje += "Error en la linea No. " + str(i) + " exiten Campos EXTRA;  Campos encontrados: "+ str(NoCampos) + "\n"
+                mensaje += "Error en la linea No. " + str(i+1) + " exiten Campos EXTRA;  Campos encontrados: "+ str(NoCampos) + "\n"
             
         #──O────────────────O─
         # Validador
@@ -38,6 +38,64 @@ def validadorcamposextra(_listaElementos):
     except Exception as e:
         VNT0.Mostrar("Error [Segmentador SEG2]: Ocurrio un error al validar campos extra o faltantes \n " + str(e))
 
+#————————————————————»✦«—————————————————————————————————————————————————#
+def validadorcodigocurso(_listaElementos,ini,titulo):
+    try:
+        mensajeerror = ""
+        #█═══════════════[ Validar es Codigo ] ══════════════════════════════════█
+        #Valida si es un numero positivo menor a o igual a 3
+        for i in range(0,len(_listaElementos)):
+            #──O────────────────O─
+            validadornum = Bvalidadornumero(_listaElementos[i][ini])
+            #Validador si es un numero
+            if(validadornum == True):
+                #──O────────────────O─
+                #Validar si es un numero Positivo
+                if(int(_listaElementos[i][ini]) > 0 ):
+                    #──O────────────────O─
+                    #Quitar ceros a la izquierda
+                    numero= int(_listaElementos[i][ini])
+                    #──O────────────────O─
+                    #Validar si es un numero de 3 digitos
+                    if(numero <= 999):
+                        None
+                    else:
+                        #──O────────────────O─
+                        #No es un codigo valido
+                        mensajeerror += "Linea " + str(i+1) + " (Error: No es un codigo Valido), "    
+                else:
+                    #──O────────────────O─
+                    #No es un positivo
+                    mensajeerror += "Linea " + str(i+1) + " (Error: No es un numero Positivo), "    
+            else:
+                #──O────────────────O─
+                #No es un numero 
+                mensajeerror += "Linea " + str(i+1) + " (Error: No es un numero), "
+        #──O────────────────O─
+        if (mensajeerror == ""):
+            #──O────────────────O─
+            #No hay errores de Codigo
+            return True
+        else:
+            #──O────────────────O─
+            VNT0.Mostrar("Error [Segmentador SEG2]: Revisar campo "+titulo+" en las siguiente lineas: " + mensajeerror)
+            return False
+                
+        
+        #──O────────────────O─
+    except Exception as e:
+        VNT0.Mostrar("Error [Segmentador SEG2]: Ocurrio un erro al validar codigo curso. \n"+str(e))
+        return None
+
+#————————————————————»✦«—————————————————————————————————————————————————##
+#Boolean #Reusable
+def Bvalidadornumero(textonum):
+    try:
+        numero = int(textonum)
+        return True
+    except Exception as e:
+        return False
+    
 
 #————————————————————»✦«—————————————————————————————————————————————————#
 def exportarValidadores(listaElementos):
@@ -45,11 +103,17 @@ def exportarValidadores(listaElementos):
     #█═══════════════[ Validar si no hay campos extra o faltantes ] ══════════════════════════════════█
     #funcion devuleve True o False
     Validadorcamposextras = validadorcamposextra(listaElementos)
-    #█═══════════════[ Validar Codigo Curso ] ══════════════════════════════════█
-    #──O────────────────O─
-    #Validador 
+    
+    #Validador Boolean si el Codigo del curso esta bien
     if (Validadorcamposextras == False):
-        print("Validando Codigo curso....")
+        #█═══════════════[ Validar Codigo Curso ] ══════════════════════════════════█
+        #──O────────────────O─
+        Validadorcodigocurso = validadorcodigocurso(listaElementos,0,"codigo carrera")
+        print("Validando Codigo curso.... ",Validadorcodigocurso)
+        #█═══════════════[ Validar Prerequisitos Curso ] ══════════════════════════════════█
+
+        #──O────────────────O─
+        
     
 
     
