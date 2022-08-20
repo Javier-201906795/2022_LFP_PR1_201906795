acc4fb6661b276e43209cb06aeb7e00c03e75963
Javier-201906795 <88503776+Javier-201906795@users.noreply.github.com>
2022-08-20 10:06:20 -0600

    Update LFP_SEG1_Segmentador_datos.py

--- LFP_SEG1_Segmentador_datos.py
+++ LFP_SEG1_Segmentador_datos.py
@@ -13,22 +13,37 @@ def exportartexto(texto):
     #se dividira el texto en los saltos de linea
     listadoSaltosLinea=texto.splitlines()
     print("lita0: ", len(listadoSaltosLinea))
-    #█═══════════════[ Separador ] ══════════════════════════════════█
+    #█═══════════════[ Separador comas ] ══════════════════════════════════█
+    #se guardara una sublistas con los elemnetos que esten antes de la coma
     global lista1
     lista1 = []
     print("lista1: ", len(lista1))
     #──O────────────────O─
     for i in range(0,(len(listadoSaltosLinea))):
-        print("numreo:",i)
         #──O────────────────O─
-        #Separ por ,
+        #Separar por ,
         lista1.append(listadoSaltosLinea[i].split(","))
-        print(lista1[i])
+        #──O────────────────O─
+#        print(lista1[i])
+#        print("================================")
+#        print(lista1)
+#        print("================================")
+#        print(lista1[0][1])
+#        print(lista1[11][2])
+
+    #█═══════════════[ Separador de punto y coma ] ══════════════════════════════════█
+    # separador para los cursos prerequisitos
+    #──O────────────────O─
+    for i in range(0,(len(listadoSaltosLinea))):
+        #──O────────────────O─
+        #Separar por ; y lo guarda en la lista 
+        #print(lista1[i][2])
+        lista1[i][2] = lista1[i][2].split(";")
+        #──O────────────────O─
+        
+#        print(lista1[11][2])
+#        print(lista1[11][2][0])
+#        print(lista1[11][2][1])
     
-    print("================================")
-    print(lista1)
-    print("================================")
-    print(lista1[0][1])
-    print(lista1[11][2])
 
     print("Evaluando")
